e721119f74e6d9efc6e235ee2115a18a1353d417
Noname <osunlong@gmail.com>
2019-05-31 17:06:04 +0700

    Resource Generate on progress

--- cubetiq/litegen/src/Generators/Resource/SimpleResourceGenerator.php
+++ cubetiq/litegen/src/Generators/Resource/SimpleResourceGenerator.php
@@ -7,6 +7,7 @@ namespace Cubetiq\Litegen\Generators\Resource;
 use Cubetiq\Litegen\Base\BaseGeneratorRepository;
 use Cubetiq\Litegen\Configuration;
 use Cubetiq\Litegen\Generators\ResourceGeneratorInterface;
+use Cubetiq\Litegen\Support\Helper;
 
 class SimpleResourceGenerator extends BaseGeneratorRepository implements ResourceGeneratorInterface
 {
@@ -14,12 +15,13 @@ class SimpleResourceGenerator extends BaseGeneratorRepository implements Resourc
 
     protected function getTargetPath()
     {
-
+        $name='';
+        return Configuration::get_project_path().'/app/Http/Resources/'.$name.'/'.$name.'Resource.php';
     }
 
     protected function getContent()
     {
-
+        return view();
     }
 
     public function parse()
@@ -28,8 +30,18 @@ class SimpleResourceGenerator extends BaseGeneratorRepository implements Resourc
         $table_names=array_keys($controllers['actions']);
         foreach ($table_names as $name)
         {
-
+            $this->table_name=Helper::studly_singular($name);
         }
         dd($table_names);
+    }
+
+    private function get_resourceConfig(){
+        $name=$this->table_name;
+        $output= Configuration::get_project_path().'/app/Http/Resources/'.$name.'/'.$name.'Resource.php';
+        $content=view()->render();
+        return [
+            "output"=>$output,
+            "content"=>$content
+        ];
     }
 }
